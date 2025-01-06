4352dfc98d82300cb3c5ecba2c3f93bef5089104
Guilherme Brito <guilherme_tabordabrito@hotmail.com>
2025-01-06 11:58:08 -0300

    Add files via upload

---
+++ Primeira/Introducao/intro.js
@@ -0,0 +1,30 @@
+console.log("Olá")
+console.log("Guilherme")
+
+
+
+
+//variáveis
+// responsáveis por guardar dados na memória do próprio compatudar e evitam repetição
+
+var nome = "Lucas"
+console.log(nome, nome, nome, nome)
+console.log(2, 4)
+let sobrenome = "Brito"
+const verdadeiro = true
+const idade = 18
+
+//tipos de dados
+//string, number, boolean
+
+var lanchonete = "Mc Donald"
+console.log(lanchonete, typeof lanchonete)
+console.log(idade, typeof idade)
+console.log(verdadeiro, typeof verdadeiro)
+
+
+//concatenação
+console.log(nome + " " + sobrenome) 
+
+
+
